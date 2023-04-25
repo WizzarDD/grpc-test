5e8aaee9ea10d26c45b1cc1648ef8ea7433b46a1
Rusanov Artur <a.rusanov@automacon.ru>
2023-04-25 15:59:05 +0300

    balances format

--- api/balances.pb.go
+++ api/balances.pb.go
@@ -20,17 +20,23 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-// The request message containing the user's name.
-type HelloRequest struct {
+// Запрос остатков
+type BalancesRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	ProductList  []int64 `protobuf:"varint,1,rep,packed,name=product_list,json=productList,proto3" json:"product_list,omitempty"` // Массив товаров
+	ShopList     []int64 `protobuf:"varint,2,rep,packed,name=shop_list,json=shopList,proto3" json:"shop_list,omitempty"`          // Массив айди магазинов
+	DeliveryDate string  `protobuf:"bytes,3,opt,name=delivery_date,json=deliveryDate,proto3" json:"delivery_date,omitempty"`      // Дата доставки
+	SlotStart    string  `protobuf:"bytes,4,opt,name=slot_start,json=slotStart,proto3" json:"slot_start,omitempty"`               // Время начала слота (H:i:s)
+	SlotFinish   string  `protobuf:"bytes,5,opt,name=slot_finish,json=slotFinish,proto3" json:"slot_finish,omitempty"`            // Время конца слота (H:i:s)
+	SlotDuring   string  `protobuf:"bytes,6,opt,name=slot_during,json=slotDuring,proto3" json:"slot_during,omitempty"`            // Длительность слота (H:i:s)
+	IsGreen      bool    `protobuf:"varint,7,opt,name=is_green,json=isGreen,proto3" json:"is_green,omitempty"`                    // Возвращать зелёные ценники?
 }
 
-func (x *HelloRequest) Reset() {
-	*x = HelloRequest{}
+func (x *BalancesRequest) Reset() {
+	*x = BalancesRequest{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_proto_balances_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -38,13 +44,13 @@ func (x *HelloRequest) Reset() {
 	}
 }
 
-func (x *HelloRequest) String() string {
+func (x *BalancesRequest) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*HelloRequest) ProtoMessage() {}
+func (*BalancesRequest) ProtoMessage() {}
 
-func (x *HelloRequest) ProtoReflect() protoreflect.Message {
+func (x *BalancesRequest) ProtoReflect() protoreflect.Message {
 	mi := &file_proto_balances_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -56,29 +62,71 @@ func (x *HelloRequest) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use HelloRequest.ProtoReflect.Descriptor instead.
-func (*HelloRequest) Descriptor() ([]byte, []int) {
+// Deprecated: Use BalancesRequest.ProtoReflect.Descriptor instead.
+func (*BalancesRequest) Descriptor() ([]byte, []int) {
 	return file_proto_balances_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *HelloRequest) GetName() string {
+func (x *BalancesRequest) GetProductList() []int64 {
 	if x != nil {
-		return x.Name
+		return x.ProductList
+	}
+	return nil
+}
+
+func (x *BalancesRequest) GetShopList() []int64 {
+	if x != nil {
+		return x.ShopList
+	}
+	return nil
+}
+
+func (x *BalancesRequest) GetDeliveryDate() string {
+	if x != nil {
+		return x.DeliveryDate
 	}
 	return ""
 }
 
-// The response message containing the greetings
-type HelloReply struct {
+func (x *BalancesRequest) GetSlotStart() string {
+	if x != nil {
+		return x.SlotStart
+	}
+	return ""
+}
+
+func (x *BalancesRequest) GetSlotFinish() string {
+	if x != nil {
+		return x.SlotFinish
+	}
+	return ""
+}
+
+func (x *BalancesRequest) GetSlotDuring() string {
+	if x != nil {
+		return x.SlotDuring
+	}
+	return ""
+}
+
+func (x *BalancesRequest) GetIsGreen() bool {
+	if x != nil {
+		return x.IsGreen
+	}
+	return false
+}
+
+// Ответ
+type BalancesResponse struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	Items []*Items `protobuf:"bytes,1,rep,name=items,proto3" json:"items,omitempty"`
 }
 
-func (x *HelloReply) Reset() {
-	*x = HelloReply{}
+func (x *BalancesResponse) Reset() {
+	*x = BalancesResponse{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_proto_balances_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -86,13 +134,13 @@ func (x *HelloReply) Reset() {
 	}
 }
 
-func (x *HelloReply) String() string {
+func (x *BalancesResponse) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*HelloReply) ProtoMessage() {}
+func (*BalancesResponse) ProtoMessage() {}
 
-func (x *HelloReply) ProtoReflect() protoreflect.Message {
+func (x *BalancesResponse) ProtoReflect() protoreflect.Message {
 	mi := &file_proto_balances_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -104,14 +152,85 @@ func (x *HelloReply) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use HelloReply.ProtoReflect.Descriptor instead.
-func (*HelloReply) Descriptor() ([]byte, []int) {
+// Deprecated: Use BalancesResponse.ProtoReflect.Descriptor instead.
+func (*BalancesResponse) Descriptor() ([]byte, []int) {
 	return file_proto_balances_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *HelloReply) GetMessage() string {
+func (x *BalancesResponse) GetItems() []*Items {
 	if x != nil {
-		return x.Message
+		return x.Items
+	}
+	return nil
+}
+
+type Items struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ost            float32 `protobuf:"fixed32,1,opt,name=ost,proto3" json:"ost,omitempty"`                                           // Кол-во остатков
+	Date           string  `protobuf:"bytes,2,opt,name=date,proto3" json:"date,omitempty"`                                           // Дата на которую пришли остатки
+	ScheduleStart  string  `protobuf:"bytes,3,opt,name=schedule_start,json=scheduleStart,proto3" json:"schedule_start,omitempty"`    // Расписание товара (время начала работы)
+	ScheduleFinish string  `protobuf:"bytes,4,opt,name=schedule_finish,json=scheduleFinish,proto3" json:"schedule_finish,omitempty"` // Расписание товара (время конца работы)
+}
+
+func (x *Items) Reset() {
+	*x = Items{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_balances_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Items) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Items) ProtoMessage() {}
+
+func (x *Items) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_balances_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Items.ProtoReflect.Descriptor instead.
+func (*Items) Descriptor() ([]byte, []int) {
+	return file_proto_balances_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *Items) GetOst() float32 {
+	if x != nil {
+		return x.Ost
+	}
+	return 0
+}
+
+func (x *Items) GetDate() string {
+	if x != nil {
+		return x.Date
+	}
+	return ""
+}
+
+func (x *Items) GetScheduleStart() string {
+	if x != nil {
+		return x.ScheduleStart
+	}
+	return ""
+}
+
+func (x *Items) GetScheduleFinish() string {
+	if x != nil {
+		return x.ScheduleFinish
 	}
 	return ""
 }
@@ -121,17 +240,39 @@ var File_proto_balances_proto protoreflect.FileDescriptor
 var file_proto_balances_proto_rawDesc = []byte{
 	0x0a, 0x14, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73,
 	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73,
-	0x22, 0x22, 0x0a, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
-	0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04,
-	0x6e, 0x61, 0x6d, 0x65, 0x22, 0x26, 0x0a, 0x0a, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x70,
-	0x6c, 0x79, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x32, 0x45, 0x0a, 0x07,
-	0x47, 0x72, 0x65, 0x65, 0x74, 0x65, 0x72, 0x12, 0x3a, 0x0a, 0x08, 0x53, 0x61, 0x79, 0x48, 0x65,
-	0x6c, 0x6c, 0x6f, 0x12, 0x16, 0x2e, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x2e, 0x48,
-	0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x14, 0x2e, 0x62, 0x61,
-	0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x70, 0x6c,
-	0x79, 0x22, 0x00, 0x42, 0x06, 0x5a, 0x04, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x22, 0xf2, 0x01, 0x0a, 0x0f, 0x42, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x52, 0x65, 0x71,
+	0x75, 0x65, 0x73, 0x74, 0x12, 0x21, 0x0a, 0x0c, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x5f,
+	0x6c, 0x69, 0x73, 0x74, 0x18, 0x01, 0x20, 0x03, 0x28, 0x03, 0x52, 0x0b, 0x70, 0x72, 0x6f, 0x64,
+	0x75, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x1b, 0x0a, 0x09, 0x73, 0x68, 0x6f, 0x70, 0x5f,
+	0x6c, 0x69, 0x73, 0x74, 0x18, 0x02, 0x20, 0x03, 0x28, 0x03, 0x52, 0x08, 0x73, 0x68, 0x6f, 0x70,
+	0x4c, 0x69, 0x73, 0x74, 0x12, 0x23, 0x0a, 0x0d, 0x64, 0x65, 0x6c, 0x69, 0x76, 0x65, 0x72, 0x79,
+	0x5f, 0x64, 0x61, 0x74, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0c, 0x64, 0x65, 0x6c,
+	0x69, 0x76, 0x65, 0x72, 0x79, 0x44, 0x61, 0x74, 0x65, 0x12, 0x1d, 0x0a, 0x0a, 0x73, 0x6c, 0x6f,
+	0x74, 0x5f, 0x73, 0x74, 0x61, 0x72, 0x74, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x73,
+	0x6c, 0x6f, 0x74, 0x53, 0x74, 0x61, 0x72, 0x74, 0x12, 0x1f, 0x0a, 0x0b, 0x73, 0x6c, 0x6f, 0x74,
+	0x5f, 0x66, 0x69, 0x6e, 0x69, 0x73, 0x68, 0x18, 0x05, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x73,
+	0x6c, 0x6f, 0x74, 0x46, 0x69, 0x6e, 0x69, 0x73, 0x68, 0x12, 0x1f, 0x0a, 0x0b, 0x73, 0x6c, 0x6f,
+	0x74, 0x5f, 0x64, 0x75, 0x72, 0x69, 0x6e, 0x67, 0x18, 0x06, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a,
+	0x73, 0x6c, 0x6f, 0x74, 0x44, 0x75, 0x72, 0x69, 0x6e, 0x67, 0x12, 0x19, 0x0a, 0x08, 0x69, 0x73,
+	0x5f, 0x67, 0x72, 0x65, 0x65, 0x6e, 0x18, 0x07, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x69, 0x73,
+	0x47, 0x72, 0x65, 0x65, 0x6e, 0x22, 0x39, 0x0a, 0x10, 0x42, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65,
+	0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x25, 0x0a, 0x05, 0x69, 0x74, 0x65,
+	0x6d, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0f, 0x2e, 0x62, 0x61, 0x6c, 0x61, 0x6e,
+	0x63, 0x65, 0x73, 0x2e, 0x49, 0x74, 0x65, 0x6d, 0x73, 0x52, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73,
+	0x22, 0x7d, 0x0a, 0x05, 0x49, 0x74, 0x65, 0x6d, 0x73, 0x12, 0x10, 0x0a, 0x03, 0x6f, 0x73, 0x74,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x02, 0x52, 0x03, 0x6f, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x64,
+	0x61, 0x74, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74, 0x65, 0x12,
+	0x25, 0x0a, 0x0e, 0x73, 0x63, 0x68, 0x65, 0x64, 0x75, 0x6c, 0x65, 0x5f, 0x73, 0x74, 0x61, 0x72,
+	0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0d, 0x73, 0x63, 0x68, 0x65, 0x64, 0x75, 0x6c,
+	0x65, 0x53, 0x74, 0x61, 0x72, 0x74, 0x12, 0x27, 0x0a, 0x0f, 0x73, 0x63, 0x68, 0x65, 0x64, 0x75,
+	0x6c, 0x65, 0x5f, 0x66, 0x69, 0x6e, 0x69, 0x73, 0x68, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x0e, 0x73, 0x63, 0x68, 0x65, 0x64, 0x75, 0x6c, 0x65, 0x46, 0x69, 0x6e, 0x69, 0x73, 0x68, 0x32,
+	0x4a, 0x0a, 0x08, 0x42, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x12, 0x3e, 0x0a, 0x03, 0x47,
+	0x65, 0x74, 0x12, 0x19, 0x2e, 0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x2e, 0x42, 0x61,
+	0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x1a, 0x2e,
+	0x62, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65, 0x73, 0x2e, 0x42, 0x61, 0x6c, 0x61, 0x6e, 0x63, 0x65,
+	0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x06, 0x5a, 0x04, 0x2f,
+	0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -146,19 +287,21 @@ func file_proto_balances_proto_rawDescGZIP() []byte {
 	return file_proto_balances_proto_rawDescData
 }
 
-var file_proto_balances_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_proto_balances_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_proto_balances_proto_goTypes = []interface{}{
-	(*HelloRequest)(nil), // 0: balances.HelloRequest
-	(*HelloReply)(nil),   // 1: balances.HelloReply
+	(*BalancesRequest)(nil),  // 0: balances.BalancesRequest
+	(*BalancesResponse)(nil), // 1: balances.BalancesResponse
+	(*Items)(nil),            // 2: balances.Items
 }
 var file_proto_balances_proto_depIdxs = []int32{
-	0, // 0: balances.Greeter.SayHello:input_type -> balances.HelloRequest
-	1, // 1: balances.Greeter.SayHello:output_type -> balances.HelloReply
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	2, // 0: balances.BalancesResponse.items:type_name -> balances.Items
+	0, // 1: balances.Balances.Get:input_type -> balances.BalancesRequest
+	1, // 2: balances.Balances.Get:output_type -> balances.BalancesResponse
+	2, // [2:3] is the sub-list for method output_type
+	1, // [1:2] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_proto_balances_proto_init() }
@@ -168,7 +311,7 @@ func file_proto_balances_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_proto_balances_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*HelloRequest); i {
+			switch v := v.(*BalancesRequest); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -180,7 +323,19 @@ func file_proto_balances_proto_init() {
 			}
 		}
 		file_proto_balances_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*HelloReply); i {
+			switch v := v.(*BalancesResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_balances_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Items); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -198,7 +353,7 @@ func file_proto_balances_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_balances_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- api/balances_grpc.pb.go
+++ api/balances_grpc.pb.go
@@ -18,88 +18,88 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// GreeterClient is the client API for Greeter service.
+// BalancesClient is the client API for Balances service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type GreeterClient interface {
-	// Sends a greeting
-	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
+type BalancesClient interface {
+	// Получение остатков
+	Get(ctx context.Context, in *BalancesRequest, opts ...grpc.CallOption) (*BalancesResponse, error)
 }
 
-type greeterClient struct {
+type balancesClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewGreeterClient(cc grpc.ClientConnInterface) GreeterClient {
-	return &greeterClient{cc}
+func NewBalancesClient(cc grpc.ClientConnInterface) BalancesClient {
+	return &balancesClient{cc}
 }
 
-func (c *greeterClient) SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error) {
-	out := new(HelloReply)
-	err := c.cc.Invoke(ctx, "/balances.Greeter/SayHello", in, out, opts...)
+func (c *balancesClient) Get(ctx context.Context, in *BalancesRequest, opts ...grpc.CallOption) (*BalancesResponse, error) {
+	out := new(BalancesResponse)
+	err := c.cc.Invoke(ctx, "/balances.Balances/Get", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// GreeterServer is the server API for Greeter service.
-// All implementations must embed UnimplementedGreeterServer
+// BalancesServer is the server API for Balances service.
+// All implementations must embed UnimplementedBalancesServer
 // for forward compatibility
-type GreeterServer interface {
-	// Sends a greeting
-	SayHello(context.Context, *HelloRequest) (*HelloReply, error)
-	mustEmbedUnimplementedGreeterServer()
+type BalancesServer interface {
+	// Получение остатков
+	Get(context.Context, *BalancesRequest) (*BalancesResponse, error)
+	mustEmbedUnimplementedBalancesServer()
 }
 
-// UnimplementedGreeterServer must be embedded to have forward compatible implementations.
-type UnimplementedGreeterServer struct {
+// UnimplementedBalancesServer must be embedded to have forward compatible implementations.
+type UnimplementedBalancesServer struct {
 }
 
-func (UnimplementedGreeterServer) SayHello(context.Context, *HelloRequest) (*HelloReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SayHello not implemented")
+func (UnimplementedBalancesServer) Get(context.Context, *BalancesRequest) (*BalancesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
 }
-func (UnimplementedGreeterServer) mustEmbedUnimplementedGreeterServer() {}
+func (UnimplementedBalancesServer) mustEmbedUnimplementedBalancesServer() {}
 
-// UnsafeGreeterServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to GreeterServer will
+// UnsafeBalancesServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to BalancesServer will
 // result in compilation errors.
-type UnsafeGreeterServer interface {
-	mustEmbedUnimplementedGreeterServer()
+type UnsafeBalancesServer interface {
+	mustEmbedUnimplementedBalancesServer()
 }
 
-func RegisterGreeterServer(s grpc.ServiceRegistrar, srv GreeterServer) {
-	s.RegisterService(&Greeter_ServiceDesc, srv)
+func RegisterBalancesServer(s grpc.ServiceRegistrar, srv BalancesServer) {
+	s.RegisterService(&Balances_ServiceDesc, srv)
 }
 
-func _Greeter_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(HelloRequest)
+func _Balances_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BalancesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GreeterServer).SayHello(ctx, in)
+		return srv.(BalancesServer).Get(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/balances.Greeter/SayHello",
+		FullMethod: "/balances.Balances/Get",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GreeterServer).SayHello(ctx, req.(*HelloRequest))
+		return srv.(BalancesServer).Get(ctx, req.(*BalancesRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Greeter_ServiceDesc is the grpc.ServiceDesc for Greeter service.
+// Balances_ServiceDesc is the grpc.ServiceDesc for Balances service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Greeter_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "balances.Greeter",
-	HandlerType: (*GreeterServer)(nil),
+var Balances_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "balances.Balances",
+	HandlerType: (*BalancesServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "SayHello",
-			Handler:    _Greeter_SayHello_Handler,
+			MethodName: "Get",
+			Handler:    _Balances_Get_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
